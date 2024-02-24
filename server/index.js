89d690fa64b8f4d9c47b418fbc2fb074da646616
Hemanth J <104612328+JayaramHemanth@users.noreply.github.com>
2024-02-24 18:36:29 +0530

    index.js

--- server/index.js
+++ server/index.js
@@ -10,7 +10,7 @@ const app = express()
 
 app.use(express.json())
 app.use(cors({
-    origin:'http://localhost:5173'
+    origin:'https://crud-operations-livid.vercel.app'
 }))
 app.use("/authentication", authRouter)
 app.use('/user',userRouter)
